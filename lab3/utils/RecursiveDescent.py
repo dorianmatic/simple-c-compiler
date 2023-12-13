3dffc848309ce0467d43b6f25c8d56ad5dcb2301
Dorian Matić <dorian.matic@fer.hr>
2023-12-13 11:50:18 +0100

    rules implementation progress

--- lab3/utils/RecursiveDescent.py
+++ lab3/utils/RecursiveDescent.py
@@ -39,6 +39,9 @@ class RecursiveDescent:
 
             return Types.CHAR, False
         elif children_names == ['NIZ_ZNAKOVA']:
+            if not Types.validate_string(node.children[0].value):
+                self._terminate('')
+
             return Types.ARRAY_CONST_CHAR, False
         elif children_names == ['L_ZAGRADA', '<izraz>', 'D_ZAGRADA']:
             return self._expression(node.children[1])
@@ -49,15 +52,129 @@ class RecursiveDescent:
         if children_names == ['<primary_expression>']:
             return self._primary_expression(node.children[0])
         elif children_names == ['<postfiks_izraz>', 'L_UGL_ZAGRADA', '<izraz>', 'D_UGL_ZAGRADA']:
-            pass
+            postfix_type, postfix_l_expression =  self._postfix_expression(node.children[0])
+
+            if not Types.is_array(postfix_type):
+                self._terminate('')
+
+            expression_type, _ = self._expression(node.children[2])
+            if not Types.is_castable(expression_type, Types.INT):
+                self._terminate('')
+
+            return Types.get_array_type(postfix_type), not Types.is_const(postfix_type)
+
         elif children_names == ['<postfiks_izraz>', 'L_ZAGRADA', 'D_ZAGRADA']:
+            # TODO
             pass
         elif children_names == ['<postfiks_izraz>', 'L_ZAGRADA', '<lista_argumenata>', 'D_ZAGRADA']:
+            # TODO
             pass
         elif children_names == ['<postfiks_izraz>', 'OP_INC'] or children_names == ['<postfiks_izraz>', 'OP_DEC']:
-            pass
-        
+            postfix_type, postfix_l_expression = self._postfix_expression(node.children[0])
 
+            if not Types.is_castable(postfix_type, Types.INT) or postfix_l_expression is False:
+                self._terminate('')
+
+            return Types.INT, False
+
+    def _argument_list(self, node: Node):
+        children_names = self._get_children_names(node)
+
+        if children_names == ['<izraz_pridruzivanja>']:
+            assigment_exp_type, _ = self._assignment_expression(node.children[0])
+
+            return [assigment_exp_type]
+        if children_names == ['<lista_argumenata>', 'ZAREZ', '<izraz_pridruzivanja>']:
+            argument_list_type, _ = self._argument_list(node.children[0])
+            assigment_exp_type, _ = self._assignment_expression(node.children[2])
+
+            return argument_list_type + assigment_exp_type
+
+    def _unary_expression(self, node: Node):
+        children_names = self._get_children_names(node)
+
+        if children_names == ['<postfiks_izraz>']:
+            return self._postfix_expression(node.children[0])
+        elif children_names == ['OP_INC', '<unary_expression>'] or children_names == ['OP_DEC', '<unary_expression>']:
+            unary_exp_type, unary_exp_l_exp = self._unary_expression(node.children[0])
+            if unary_exp_l_exp == False or not Types.is_castable(unary_exp_type, Types.INT):
+                self._terminate('')
+
+            return Types.INT, False
+        elif children_names == ['<unarni_operator>', '<cast_izraz>']:
+            cast_exp_type, _ = self._cast_expression(node.children[1])
+            if not Types.is_castable(cast_exp_type, Types.INT):
+                self._terminate('')
+
+            return Types.INT, False
+
+    def _cast_expression(self, node: Node):
+        children_names = self._get_children_names(node)
+
+        if children_names == ['<unary_expression>']:
+            return self._unary_expression(node.children[0])
+        elif children_names == ['L_ZAGRADA', '<ime_tipa>', 'D_ZAGRADA', '<cast_izraz>']:
+            type_name_type, _ = self._type_name(node.children[1])
+            cast_exp_type, _ = self._cast_expression(node.children[3])
+
+            if not Types.is_explicitly_castable(cast_exp_type, type_name_type):
+                self._terminate('')
+
+            return type_name_type, False
+
+    def _type_name(self, node: Node):
+        children_names = self._get_children_names(node)
+
+        if children_names == ['<specifikator_tipa>']:
+            return self._type_specifier(node.children[0])
+        elif children_names == ['KR_CONST', '<specifikator_tipa>']:
+            type_specifier_type = self._type_specifier(node.children[1])
+            if type_specifier_type == Types.VOID:
+                self._terminate('')
+
+            return Types.to_const(type_specifier_type)
+
+
+    def _type_specifier(self, node: Node):
+        children_names = self._get_children_names(node)
+
+        if children_names == ['KR_VOID']:
+            return Types.VOID
+        elif children_names == ['KR_CHAR']:
+            return Types.CHAR
+        elif children_names == ['KR_INT']:
+            return Types.INT
+
+    def _multiplicative_expression(self, node: Node):
+        children_names = self._get_children_names(node)
+
+        if children_names == ['<cast_expression>']:
+            return self._cast_expression(node.children[0])
+        elif (children_names == ['<multiplikativni_izraz>', 'OP_PUTA', '<cast_izraz>'] or
+              children_names == ['<multiplikativni_izraz>', 'OP_DIJELI', '<cast_izraz>'] or
+              children_names == ['<multiplikativni_izraz>', 'OP_MOD', '<cast_izraz>']):
+            multiplicative_exp_type, _ = self._multiplicative_expression(node.children[0])
+            if not Types.is_castable(multiplicative_exp_type, Types.INT):
+                self._terminate('')
+
+            cast_exp_type, _ = self._cast_expression(node.children[2])
+            if not Types.is_castable(cast_exp_type, Types.INT):
+                self._terminate('')
+
+            return Types.INT, False
+
+    def _additive_expression(self, node: Node):
+        children_names = self._get_children_names(node)
+
+        if children_names == ['<multiplikativni_izraz>']:
+            pass
+        elif (children_names == ['<aditivni_izraz>', 'PLUS', '<multiplikativni_izraz>'] or
+              children_names == ['<aditivni_izraz>', 'MINUS', '<multiplikativni_izraz>']):
+            pass
+
+
+    def _assignment_expression(self, node: Node):
+        pass
 
     def _expression(self, node: Node):
         pass
--- lab3/utils/types.py
+++ lab3/utils/types.py
@@ -9,6 +9,20 @@ class Types:
     ARRAY_CONST_INT = 'niz(const(int))'
     VOID = 'void'
 
+    CASTS = [
+        (CONST_INT, INT), (INT, CONST_INT),
+        (CONST_CHAR, CHAR), (CHAR, CONST_CHAR),
+        (CHAR, INT), (CHAR, CONST_INT),
+        (CONST_CHAR, INT), (CONST_CHAR, CONST_INT),
+        (ARRAY_INT, ARRAY_CONST_INT), (ARRAY_CHAR, ARRAY_CONST_CHAR)
+    ]
+
+    # TODO: Can one cast to CONST_INT?
+    EXPLICIT_CASTS = [
+        (CHAR, INT), (CONST_CHAR, INT)
+    ]
+
+
     @staticmethod
     def validate_int(value):
         return -2147483648 <= value <= 2147483647
@@ -17,3 +31,31 @@ class Types:
     def validate_char(value):
         # TODO: Finish char validation implementation
         return 0 <= value <= 255
+
+    @staticmethod
+    def validate_string(value):
+        pass
+
+    @classmethod
+    def is_castable(cls, from_type, to_type):
+        return (from_type, to_type) in cls.CASTS
+
+    @classmethod
+    def is_explicitly_castable(cls, from_type, to_type):
+        return (from_type, to_type) in cls.EXPLICIT_CASTS
+
+    @classmethod
+    def is_array(cls, var_type):
+        return 'niz' in var_type
+
+    @classmethod
+    def get_array_type(cls, var_type):
+        return var_type[4:-1]
+
+    @classmethod
+    def is_const(cls, var_type):
+        return 'const' in var_type
+
+    @staticmethod
+    def to_const(var_type):
+        return f'const(#{var_type})'
